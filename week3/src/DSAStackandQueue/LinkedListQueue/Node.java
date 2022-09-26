f25fb46e00b9a7a242cd400d24168e24d8c95c70
Sonhr1403 <110454300+Sonhr1403@users.noreply.github.com>
2022-09-27 06:16:37 +0700

    Add files via upload

---
+++ week3/src/DSAStackandQueue/LinkedListQueue/Node.java
@@ -0,0 +1,11 @@
+package DSAStackandQueue.LinkedListQueue;
+
+class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
